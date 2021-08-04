4b8114eb784c0554aee073ccee86f1da98b1de68
City of Vernonia GIS <gis@vernonia-or.gov>
2021-08-04 07:30:56 -0700

    add more layers and update deps

--- src/app.ts
+++ src/app.ts
@@ -17,9 +17,11 @@ import BingMapsLayer from '@arcgis/core/layers/BingMapsLayer';
 import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import MapImageLayer from '@arcgis/core/layers/MapImageLayer';
+import GroupLayer from '@arcgis/core/layers/GroupLayer';
 
 // popups
 import TaxLotPopup from './core/popups/TaxLotPopup';
+// import TaxMapPopup from './core/widgets/TaxMaps/TaxMapPopup';
 
 // search
 import SearchViewModel from '@arcgis/core/widgets/Search/SearchViewModel';
@@ -33,6 +35,7 @@ import MarkupViewModel from './core/viewModels/MarkupViewModel';
 import LayerListLegend from './core/widgets/LayerListLegend';
 import Measure from './core/widgets/Measure';
 import Print from './core/widgets/Print';
+// import TaxMaps from './core/widgets/TaxMaps';
 
 // config portal and auth
 esriConfig.portalUrl = 'https://gisportal.vernonia-or.gov/portal';
@@ -55,8 +58,77 @@ const oAuthViewModel = new OAuthViewModel({
   }),
 });
 
+/**
+ * load app after auth successfully loaded
+ */
 const loadApp = (): void => {
   // layers
+  const taxMaps = new MapImageLayer({
+    portalItem: {
+      id: '9c3b13af5a0b4fe7b57316d14259f893',
+    },
+    opacity: 0.5,
+    listMode: 'hide',
+    visible: true, // make sure
+  });
+
+  const taxMapBoundaries = new FeatureLayer({
+    portalItem: {
+      id: '5bbd874a1b4f4674a03ce5ba25e08bf2',
+    },
+    title: 'Tax Maps',
+    visible: false,
+    // popupTemplate: new TaxMapPopup(),
+  });
+
+  const soils = new FeatureLayer({
+    visible: false,
+    portalItem: {
+      id: '1bb66a7c50714426967cadee1a753e0c',
+    },
+  });
+
+  const wetlands = new GroupLayer({
+    visible: false,
+    title: 'Wetlands',
+    layers: [
+      new FeatureLayer({
+        visible: false,
+        title: 'Vernonia Artificial Wetlands',
+        portalItem: {
+          id: '73ba1a7ff19e42b38b3910b6d02beafb',
+        },
+      }),
+      new FeatureLayer({
+        visible: false,
+        title: 'Vernonia LWI 2001',
+        portalItem: {
+          id: '5452cb87c2934546aa0ac48653231201',
+        },
+      }),
+      new FeatureLayer({
+        title: 'Oregon Wetlands',
+        portalItem: {
+          id: 'fa8d00ea829c40979b882d2af3b6ae76',
+        },
+      }),
+    ],
+  });
+
+  const zoning = new FeatureLayer({
+    visible: false,
+    portalItem: {
+      id: '4e24a499afc54717bd1695f9143bed44',
+    },
+  });
+
+  const buildingFootprints = new FeatureLayer({
+    visible: false,
+    portalItem: {
+      id: 'ffb2bf53ea0b4223bf271e560acb3a56',
+    },
+  });
+
   const cityLimits = new FeatureLayer({
     portalItem: {
       id: 'eb0c7507611e44b7923dd1c0167e3b92',
@@ -74,7 +146,24 @@ const loadApp = (): void => {
     portalItem: {
       id: 'a6063eb199e640e0bbc2d5ceca23de9a',
     },
+    opacity: 0.75,
     popupTemplate: new TaxLotPopup(),
+  });
+
+  const floodHazard = new GroupLayer({
+    portalItem: {
+      id: '027c75b51e1048eaa0cdf760218ccdfc',
+    },
+    title: 'Flood Hazard',
+    visible: false,
+  });
+  // the api doesn't apply default visibility
+  floodHazard.when(() => {
+    floodHazard.layers.forEach((layer, idx) => {
+      if (idx !== 0) {
+        layer.visible = false;
+      }
+    });
   });
 
   const searchViewModel = new SearchViewModel({
@@ -140,7 +229,18 @@ const loadApp = (): void => {
           id: 'f36cd213cc934d2391f58f389fc9eaec',
         },
       }),
-      layers: [taxLots, ugb, cityLimits],
+      layers: [
+        taxMaps,
+        soils,
+        wetlands,
+        zoning,
+        buildingFootprints,
+        floodHazard,
+        taxLots,
+        ugb,
+        cityLimits,
+        taxMapBoundaries,
+      ],
       ground: 'world-elevation',
     }),
     zoom: 15,
@@ -200,6 +300,15 @@ const loadApp = (): void => {
         icon: 'print',
         text: 'Print',
       },
+      // {
+      //   widget: new TaxMaps({
+      //     view,
+      //     featureLayer: taxMapBoundaries,
+      //     mapImageLayer: taxMaps,
+      //   }),
+      //   icon: 'map',
+      //   text: 'Tax Maps',
+      // },
     ],
   });
 
